2811daf3b955f449c562728ba421c43dd554fc8b
William Kasafir <91284390+William-Kasafir@users.noreply.github.com>
2022-11-30 16:31:49 +0000

    Update Problem23.hs

--- 21to28/Problem23.hs
+++ 21to28/Problem23.hs
@@ -1,6 +1,7 @@
 import System.Random
 import Control.Monad (replicateM)
 
+-- Solution included in the problem sets.
 rnd_select :: [a] -> Int -> IO [a] -- We are using the IO() monad
 rnd_select [] _ = return [] -- If we cannot select any elements, we are fucked.
 rnd_select l  n  
@@ -8,3 +9,13 @@ rnd_select l  n
     | otherwise = do pos <- replicateM n $  -- We perform the following action three times.
                                getStdRandom $ randomR (0, (length l)-1) -- Generate a random number.
                      return [l!!p | p <- pos] -- return a list of the random elements we got.
+
+-- My own solution using randomRs.
+rnd_select :: [a] -> Int -> IO [a]
+rnd_select [] n = return []
+rnd_select xs n | n < 0 = error "What the fuck is this?"
+                | otherwise = do
+                           gen <- newStdGen
+                           let positions = take 3 (randomRs (0, (length xs)-1) gen)
+                           return [xs !! z | z <- positions]
+~                                                                 
